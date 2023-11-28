c81daa8e08bd37c63f6daa291ae287562b708dfc
PabloRibeiro-Br <75342853+PabloRibeiro-Br@users.noreply.github.com>
2023-11-27 21:10:16 -0300

    daw

---
+++ src/dtcCodigos.js
@@ -0,0 +1,14 @@
+const dtcCodigos = [
+    { 
+        id: 1, 
+        title: "171", 
+        description: "O Carro é uma merda, joga fora essa bosta.",
+        correction:"aqui é onde o cara inventa histórinha pra empurrar uma peça nova"
+    },
+    { 
+        id: 1, 
+        title: "172", 
+        description: "Esseo código significa que fudeu, vende essa merda para um louco, e larga mão desta bomba.",
+        correction:"aqui é onde o cara inventa histórinha pra empurrar uma peça nova"
+    }
+]
